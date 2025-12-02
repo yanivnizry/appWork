7983b754f0582b528c28bcb76e85cddf21564928
Yaniv Nizry <yanivnizry@gmail.com>
2025-12-02 15:18:28 +0200

    bug fix

--- services/apis/index.ts
+++ services/apis/index.ts
@@ -2,13 +2,13 @@ import { axiosClient } from './axiosClient';
 import { config } from '../config';
 
 export const fetchTimeZones = async () => {
-  if (!config.TIMEZONEDB_API_KEY) {
+  if (!config.EXPO_PUBLIC_TIMEZONEDB_API_KEY) {
     throw new Error('Missing API key');
   }
 
   const response = await axiosClient.get('/list-time-zone', {
     params: {
-      key: config.TIMEZONEDB_API_KEY,
+      key: config.EXPO_PUBLIC_TIMEZONEDB_API_KEY,
       format: 'json',
     },
   });
